02a50d5518962fd888aad4663f3b9c3fdf1f758b
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-11 23:00:36 +0200

    create structure project and install new dependencies

---
+++ src/components/view/VerificarEntradaComponent.tsx
@@ -0,0 +1,7 @@
+export default function VerificarEntradaComponent() {
+  return (
+    <div>
+      <h1>Verificar Entrada</h1>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import "./index.css"
+import { store } from "./store/store.ts"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  //<React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  //</React.StrictMode>
 )
