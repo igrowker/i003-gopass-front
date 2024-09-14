131cf36ec5aef2fd1b652306c53f4739ba8dee87
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-14 11:46:12 +0200

    rename file UltimasEntradasComponent #5

--- src/components/core/ComprarReventaComponent/UltimasEntradasComponent.tsx
+++ src/components/core/ComprarReventaComponent/UltimasEntradasComponent.tsx
@@ -1,10 +1,10 @@
 import Button from "../Button/Button"
 
-export default function ComprarReventaComponent() {
+export default function UltimasEntradasComponent() {
   return (
     <>
-      <div className="flex justify-center p-2 ">
-        <div className="card bg-base-100 w-96 shadow-2xl ">
+      <div className="flex justify-center p-2">
+        <div className="card w-96 bg-base-100 shadow-2xl">
           <figure>
             <img
               src="https://images.unsplash.com/photo-1434648957308-5e6a859697e8?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -12,11 +12,11 @@ export default function ComprarReventaComponent() {
             />
           </figure>
           <div className="card-body pb-4">
-            <h2 className="card-title font-bold text-xl">RACING - SAN LORENZO</h2>
-            <p className="font-medium text-[1.1rem]">18 Feb - 18:00 EN AVELLANEDA</p>
-            <p className="font-medium text-[1.3rem]">$10</p>
+            <h2 className="card-title text-xl font-bold">RACING - SAN LORENZO</h2>
+            <p className="text-[1.1rem] font-medium">18 Feb - 18:00 EN AVELLANEDA</p>
+            <p className="text-[1.3rem] font-medium">$10</p>
             <div className="card-actions justify-end">
-              <Button className="btn btn-primary bg-customGreen hover:bg-customBlack relative -mt-8">Buy Now</Button>
+              <Button className="btn btn-primary relative -mt-8 bg-customGreen hover:bg-customBlack">Buy Now</Button>
             </div>
           </div>
         </div>
