15cd488b9ba76ff59924f2418b215fc4d029b931
Ezefeola <ezefeola@gmail.com>
2024-10-05 09:07:50 -0300

    ultimos cambios

--- src/api/axios-config.ts
+++ src/api/axios-config.ts
@@ -1,9 +1,22 @@
-import axios from "axios"
+import axios, { InternalAxiosRequestConfig } from "axios"
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${sessionStorage.getItem("token")}`
+  //  Authorization: `Bearer ${sessionStorage.getItem("token")}`
   }
 })
+
+
+const requestInterceptor = (config: InternalAxiosRequestConfig) => {
+  if (config.url === '/login') return config;
+  const tokenInfo = sessionStorage.getItem('token');
+
+  config.headers.Authorization = `Bearer ${tokenInfo}`;
+  
+  return config;
+};
+
+
+httpClient.interceptors.request.use(requestInterceptor)
