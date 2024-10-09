45b0e597d4cf8aa99aa0c1b2196155351d3ce3b5
j0sep0z0 <j0sep0z0@outlook.com>
2024-10-09 22:24:13 +0200

    Refactor getUserTicketsSold API endpoint URL

--- src/service/getUserTicketsSold.ts
+++ src/service/getUserTicketsSold.ts
@@ -5,7 +5,7 @@ import { httpClient } from "../api/axios-config"
 
 export const getUserTicketsSold = async () => {
   try {
-    const response = await httpClient.get(`/Usuario/obtener/usuario/entradas-reventa`)
+    const response = await httpClient.get(`/Usuario/obtener-usuario-entradas-reventa`)
     return response.data
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
