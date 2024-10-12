d9af5db683207a2900996679a393d0284905ce78
j0sep0z0 <j0sep0z0@outlook.com>
2024-10-12 22:18:27 +0200

    Add refactor AboutUs and HelpSupport

--- src/privateRoutes/PrivateRoutes.tsx
+++ src/privateRoutes/PrivateRoutes.tsx
@@ -1,19 +1,21 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 
+import { AboutUs } from "../app/pages/AboutUs"
 import AllTickets from "../app/pages/AllTickets"
 import ComprarEntrada from "../app/pages/BuyEntryPage"
 import Contact from "../app/pages/ContactPage"
+import { HelpSupport } from "../app/pages/HelpSupport"
 import LandingView from "../app/pages/LandingViewPage"
 import { MyTickets } from "../app/pages/MyTickets"
 import { MyTicketsSold } from "../app/pages/MyTicketsSold"
 import { PageNotFound } from "../app/pages/PageNotFound"
 import PagarEntrada from "../app/pages/PayEntryPage"
+import PhoneCode from "../app/pages/PhoneCode"
 import VenderEntrada from "../app/pages/SellEntryPage"
 import Ticket from "../app/pages/TicketPage"
 import UserProfile from "../app/pages/UserProfilePage"
 import VerifyEntryPage from "../app/pages/VerifyEntryPage"
-import PhoneCode from "../app/pages/PhoneCode"
 import PrivateRoute from "./PrivateRoute"
 
 const PrivateRoutes: React.FC = () => {
@@ -24,6 +26,8 @@ const PrivateRoutes: React.FC = () => {
       <Route path="/comprar-entrada" element={<PrivateRoute element={<ComprarEntrada />} />} />
       <Route path="/pagar-entrada" element={<PrivateRoute element={<PagarEntrada />} />} />
       <Route path="/contact" element={<PrivateRoute element={<Contact />} />} />
+      <Route path="/help-support" element={<PrivateRoute element={<HelpSupport />} />} />
+      <Route path="/about-us" element={<PrivateRoute element={<AboutUs />} />} />
       <Route path="/ticket" element={<PrivateRoute element={<Ticket />} />} />
       <Route path="/verificar-entrada" element={<PrivateRoute element={<VerifyEntryPage />} />} />
       <Route path="/vender-entrada" element={<PrivateRoute element={<VenderEntrada />} />} />
